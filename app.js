b602d21c93e170a6a8a101956af7ef9294c513a1
Donya Shirazi <donya_rezaii97@yahoo.com>
2022-11-21 00:26:51 +0330

    recent-changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// const { layerGroup } = require("fontawesome");
+const { layerGroup } = require("fontawesome");
 
-let submit = document.querySelector("#submit");
+const submit = document.querySelector("#submit");
 
 submit.onclick = function () {
   var formData = new FormData(document.querySelector("#blog_form"));
@@ -16,3 +16,26 @@ submit.onclick = function () {
   }
   console.log(myNewObject);
 };
+
+const getTodos = (callBack) => {
+  const apiRequest = new XMLHttpRequest();
+
+  apiRequest.addEventListener("readystatechange", () => {
+    // console.log(apiRequest, apiRequest.readyState);
+    if (apiRequest.readyState === 4 && apiRequest.status === 200) {
+      callBack(undefined, apiRequest.responseText);
+    } else if (apiRequest.readyState === 4) {
+      callBack("could not fetch the data", undefined);
+    }
+  });
+};
+apiRequest.open("GET", "https://jsonplaceholder.typicode.com/todoss/");
+apiRequest.send();
+
+getTodos((err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
