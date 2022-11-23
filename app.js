b27c5c41bf141f52cca14b749582ed88a0e0fe61
Donya Shirazi <donya_rezaii97@yahoo.com>
2022-11-23 15:05:47 +0330

    API/fetch

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const { layerGroup } = require("fontawesome");
+// const { layerGroup } = require("fontawesome");
+
+// const { firstAid } = require("fontawesome");
 
 const submit = document.querySelector("#submit");
 
@@ -17,25 +19,75 @@ submit.onclick = function () {
   console.log(myNewObject);
 };
 
-const getTodos = (callBack) => {
-  const apiRequest = new XMLHttpRequest();
+const getTodos = async () => {
+  let response = await fetch("todos/seconds.json");
+  let data = await response.json();
 
-  apiRequest.addEventListener("readystatechange", () => {
-    // console.log(apiRequest, apiRequest.readyState);
-    if (apiRequest.readyState === 4 && apiRequest.status === 200) {
-      callBack(undefined, apiRequest.responseText);
-    } else if (apiRequest.readyState === 4) {
-      callBack("could not fetch the data", undefined);
-    }
-  });
-};
-apiRequest.open("GET", "https://jsonplaceholder.typicode.com/todoss/");
-apiRequest.send();
+  let responseOne = await fetch("todos/third.json");
+  let data1 = await responseOne.json();
 
-getTodos((err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
+  if (response.status !== 200 && responseOne.status !== 200) {
+    throw new Error("cant fetch data");
   }
-});
+
+  return [data, data1];
+};
+
+getTodos()
+  .then((response) => {
+    console.log("resolved:", response);
+  })
+  .catch((err) => console.log("rejected:", err.message));
+
+// const getTodos = (resource) => {
+//   return new Promise((resolve, reject) => {
+//     const apiRequest = new XMLHttpRequest();
+
+//     apiRequest.addEventListener("readystatechange", () => {
+//       if (apiRequest.readyState === 4 && apiRequest.status === 200) {
+//         const data = JSON.parse(apiRequest.responseText);
+//         resolve(data);
+//       } else if (apiRequest.readyState === 4) {
+//         reject("data rejected");
+//       }
+//     });
+
+//     // "https://jsonplaceholder.typicode.com/todos/"
+//     apiRequest.open("GET", resource);
+//     apiRequest.send();
+//   });
+// };
+
+// getTodos("todos/first.json")
+//   .then((data) => {
+//     console.log("promise fullfiled:", data);
+//     return getTodos("todos/second.json");
+//   })
+//   .then((data) => {
+//     console.log("promise fullfiled:", data);
+//     return getTodos("todos/third.json");
+//   })
+//   .then((data) => console.log("promise fullfiled:", data))
+//   .catch((err) => {
+//     console.log("error!!!", err);
+//   });
+
+// getTodos("todos/first.json", (err, data) => {
+//   console.log(data);
+//   getTodos("todos/second.json", (err, data) => {
+//     console.log(data);
+//     getTodos("todos/third.json", (err, data) => {
+//       console.log(data);
+//     });
+//   });
+// });
+
+// fetch("todos/first.json")
+//   .then((response) => {
+//     console.log("resolved", response);
+//     return response.json();
+//   })
+//   .then((data) => console.log(data))
+//   .catch((err) => {
+//     console.log("error!!!", err);
+//   });
